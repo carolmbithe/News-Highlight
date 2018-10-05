1c3db8a7820270f148b42247ac96b64062ca13f8
carolmbithe <carombithe97@gmail.com>
2018-10-05 11:21:25 +0300

    added a dynamic route with a newssource() view function

--- app/views.py
+++ app/views.py
@@ -6,4 +6,15 @@ def index():
     """
     View root page function that returns the index page and its data
     """
-    return render_template('index.html')
+
+    heading='WELCOME TO NEWSHIGHLIGHT'
+    return render_template('index.html',heading=heading)
+
+@app.route('/newssource/<int_newssource_id>')
+def newssource(newssource_id):
+    """
+    View movie page function that returns the news sources and their details
+    """
+    return render_template('newssource.html',id = newssource_id)
+
+    
